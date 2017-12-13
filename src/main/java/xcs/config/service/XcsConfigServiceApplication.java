bdc498e0b7bf74038fc4f051a0ab223314dc2a8c
Bhuwan Upadhyay <bhuwan.upadhyay49@gmail.com>
2017-12-13 22:42:04 +0400

    #card-6150485

---
+++ src/main/java/xcs/config/service/XcsConfigServiceApplication.java
@@ -0,0 +1,12 @@
+package xcs.config.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class XcsConfigServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(XcsConfigServiceApplication.class, args);
+	}
+}
