0391dcf0424332af00901883419f8e1236f6b0e1
Bhuwan Upadhyay <bhuwan.upadhyay49@gmail.com>
2017-12-26 09:09:08 +0400

    Baby.M1

--- src/main/java/xcs/config/service/XcsConfigServiceApplication.java
+++ src/main/java/xcs/config/service/XcsConfigServiceApplication.java
@@ -2,11 +2,13 @@ package xcs.config.service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class XcsConfigServiceApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(XcsConfigServiceApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(XcsConfigServiceApplication.class, args);
+    }
 }
